61a14bfdbe3e9fa97bb1aaa07bcb3031c5bfea10
nishisharma <nishi200714sharma@gmail.com>
2026-03-23 22:45:09 +0530

    add css clean error

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  basePath: process.env.GITHUB_ACTIONS && process.env.GITHUB_ACTIONS !== 'false' ? '/Virtual-memory-OS' : '',
 };
 
 export default nextConfig;
